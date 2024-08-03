1eb455f41506917d9bd79be08313186a0bd0e556
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-08-03 16:35:14 +0300

    streamlit & tree-sitter-languages wheels from PyPI

--- flake.nix
+++ flake.nix
@@ -74,20 +74,30 @@
             tree-sitter-languages
           ];
         };
-        streamlit = pyPkgs.buildPythonPackage {
+        streamlit = pyPkgs.buildPythonPackage rec {
           pname = "streamlit";
           version = "1.2.0";
           format = "wheel";
-          src = ./streamlit-1.34.0-py2.py3-none-any.whl;
+          src = pyPkgs.fetchPypi {
+            inherit pname version format;
+            sha256 = "sha256-NoRvVpc/AZyYCmVzn81rYhWRoMWq4crulZsjixQy67c=";
+          };
           propagatedBuildInputs = with pyPkgs; [blinker tornado];
+          doCheck = false;
         };
-        tree-sitter-languages = pyPkgs.buildPythonPackage {
+        tree-sitter-languages = pyPkgs.buildPythonPackage rec {
           pname = "tree_sitter_languages";
           version = "1.10.2";
           format = "wheel";
-          src =
-            ./tree_sitter_languages-1.10.2-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl;
+          src = pkgs.fetchPypi {
+            inherit pname version format;
+            python = "cp312";
+            abi = "cp312";
+            platform = "manylinux_2_17_x86_64.manylinux2014_x86_64";
+            sha256 = "sha256-bS8c0dG91lMy+cK2fUnc8UjPHe11KFHRWaw+XuT00mA=";
+          };
           propagatedBuildInputs = with pyPkgs; [tree-sitter];
+          doCheck = false;
         };
       in {packages = {default = aider;};};
     };
